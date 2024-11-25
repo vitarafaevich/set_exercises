5197cbc5efc750bf6f707c9013581dba74b2162a
vitarafavich <v.rafaevich@g.nsu.ru>
2024-11-25 19:57:50 +0700

    1 - 3

--- 2.py
+++ 2.py
@@ -3,3 +3,4 @@ comm = set(input('enter the first student courses: ').split())
 for i in range(2, num + 1):
     comm&=set(input(f'enter the {i} student courses choice: ').split())
 print(f'the number of courses everybody choose {len(comm)}')
+
