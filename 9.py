ad9ce77940b4846e23f213ae84c4051661c321d6
vitarafavich <v.rafaevich@g.nsu.ru>
2024-11-27 07:37:11 +0700

    1 - 3

--- 9.py
+++ 9.py
@@ -2,7 +2,7 @@ from itertools import combinations
 
 def subsets(nums, k):
     k_elmnt = set(combinations(nums, k))
-    print(k_elmnt)
+    print(* sorted(k_elmnt))
 
 numbers = set(int(i) for i in input('enter some numbers: ').split())
 kk = int(input('enter the subset lenght: '))
